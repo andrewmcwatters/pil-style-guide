1d7aa48f2ae777ce15e9c9e2d5e57109dcd37474
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-26 22:07:40 -0700

    1 – Getting Started
    https://www.lua.org/pil/1.html

--- style.lua
+++ style.lua
@@ -15,7 +15,29 @@ print("Hello World")         --> Hello World
 -- "The notation --> shows the output of a statement or, occasionally, the
 --  result of an expression:"
 
--- space before and after arithmetic operators
+-- ~~space before and after arithmetic operators~~
 
 print(10)     --> 10
 13 + 3        --> 16
+
+-- https://www.lua.org/pil/1.html
+
+-- space after function name
+-- no space before or after parameters
+-- 2-space indentation
+-- no parentheses around conditionals
+-- no space before or after arithmetic operators using a number
+-- space before and after assignment operator
+
+-- defines a factorial function
+function fact (n)
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n-1)
+  end
+end
+
+print("enter a number:")
+a = io.read("*number")        -- read a number
+print(fact(a))
