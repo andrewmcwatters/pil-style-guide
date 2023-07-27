850928d8ba2e985a6078ea7f6debb6960b326543
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-26 21:55:15 -0700

    Create style.lua

---
+++ style.lua
@@ -0,0 +1,21 @@
+-- https://www.lua.org/pil/p1.3.html
+
+-- "The book encloses "literal strings" between double quotes and single
+--  characters, like `a´, between single quotes. Strings that are used as
+--  patterns are also enclosed between single quotes, like '[%w_]*'."
+
+-- space before comments
+-- comments are in lowercase and do not end with punctuation
+-- no space before or after arguments
+-- no semicolons
+
+-- program "Hello World"
+print("Hello World")         --> Hello World
+
+-- "The notation --> shows the output of a statement or, occasionally, the
+--  result of an expression:"
+
+-- space before and after arithmetic operators
+
+print(10)     --> 10
+13 + 3        --> 16
